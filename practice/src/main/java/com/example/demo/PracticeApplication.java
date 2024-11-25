ddd7be3aa3c1b820a69d31b61ff24d6f5b0fad82
Tajima0401 <s.tajima.cingroup2012@gmail.com>
2024-11-25 22:25:16 +0900

    ソースコードの変更

--- practice/src/main/java/com/example/demo/PracticeApplication.java
+++ practice/src/main/java/com/example/demo/PracticeApplication.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // これが必要です
 public class PracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeApplication.class, args);  // Spring Boot アプリケーションの起動
+    }
 
 }
